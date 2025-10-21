14add6f757fe9d4b334ed0a027d0b73a4bb5cc8e
di <1426567242@qq.com>
2025-10-21 19:36:12 +0800

    pngquant

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// 关于child_process模块的exec方法
-// 创建一个文件夹 进入目录 写一个test.js文件，内容如下：
-import { execFile } from 'child_process';
-import { resolve } from 'path';
+import { exec } from 'child_process';
+import Pngquant from 'pngquant';
 
-// exec方法用于执行一个shell命令，并且可以获取命令的输出结果
-execFile((resolve(process.cwd(), 'index.sh')), (error, stdout, stderr) => {
-    if (error) {
-        console.error(`执行出错: ${error}`);
-        return;
-    }
-    console.log(`标准输出:\n${stdout}`);
-    console.error(`标准错误输出:\n${stderr}`);
-})
+const pngquant = new Pngquant();
+
+pngquant.compress('73kb.png', { quality: [0.6, 0.8] })
+  .then(result => {
+    console.log('Compression successful');
+  })
+  .catch(error => {
+    console.error(`Error: ${error.message}`);
+  });
+
